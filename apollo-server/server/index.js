4a4054bba167ebebacde59f393e40a40ca7a1971
Yauheni Pliuta <enhorme@gmail.com>
2023-03-16 14:14:24 +0100

    test server done

--- apollo-server/server/index.js
+++ apollo-server/server/index.js
@@ -2,16 +2,27 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import resolvers from './resolvers.js';
 import typeDefs from './typeDefs.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose'
 
 async function initServer() {
     const app = express();
+    app.use(cors());
+    dotenv.config();
     const apolloServer = new ApolloServer({typeDefs,resolvers});
     await apolloServer.start();
     apolloServer.applyMiddleware({app})
     app.use((req,res)=>{
         res.send('server is started')
-    })
-    const PORT = process.env.PORT || 5000
+    });
+    const PORT = process.env.PORT || 5000;
+    try {
+       await mongoose.connect(process.env.mongodb)
+        console.log('Connected to MONGODB')
+    } catch (e) {
+         console.log(e)
+    }
     app.listen(PORT, ()=>console.log(`Server is running on PORT ${PORT}`))
 }
 
--- apollo-server/server/resolvers.js
+++ apollo-server/server/resolvers.js
@@ -1,9 +1,37 @@
+import Todo from './models/Todo.js';
+
 const resolvers = {
     Query:{
         welcome:()=>{
             return 'welcome to todo'
+        },
+        getTodos:async ()=> await Todo.find(),
+        getTodo:async (root,{id})=>await Todo.findById(id)
+    },
+    Mutation:{
+        addTodo:async (root,args)=>{
+             const newTodo = new Todo({title: args.title, detail: args.detail, date: args.date});
+             await newTodo.save();
+             return newTodo
+        },
+        deleteTodo:async (root, {id})=>{
+            await Todo.findByIdAndDelete(id)
+            return 'Todo has deleted'
+        },
+        updateTodo:async (root, args)=>{
+            const updateTodo = {}
+
+            for(let [key,value] of Object.entries(args)) {
+                if(value !== undefined) {
+                   updateTodo[key] = value
+                }
+            }
+            const todo = await Todo.findByIdAndUpdate(args.id,updateTodo, {new:true})
+            return todo;
         }
+
     }
+                                                    
 }
 
 export default resolvers
